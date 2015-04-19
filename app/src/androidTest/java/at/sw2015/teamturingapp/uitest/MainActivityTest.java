3b755800134c07b0a335f1fcf39d1129093a61ac
VanessaStanje <vanessastanje@gmail.com>
2015-04-19 12:58:30 +0200

    [LG,VS] Setting up Test Environment

---
+++ app/src/androidTest/java/at/sw2015/teamturingapp/uitest/MainActivityTest.java
@@ -0,0 +1,28 @@
+package at.sw2015.teamturingapp.uitest;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.robotium.solo.Solo;
+
+import junit.framework.TestCase;
+
+import at.sw2015.teamturingapp.MainActivity;
+
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    private Solo mySolo;
+
+    public MainActivityTest(){
+        super(MainActivity.class);
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+        mySolo = new Solo(getInstrumentation(), getActivity());
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+}
