0a4831c8a0c62cae660d4294036d9d1ef72dca15
VanessaStanje <vanessastanje@gmail.com>
2015-04-20 20:44:45 +0200

    [LG,VS] (test) testStartView

--- app/src/androidTest/java/at/sw2015/teamturingapp/uitest/MainActivityTest.java
+++ app/src/androidTest/java/at/sw2015/teamturingapp/uitest/MainActivityTest.java
@@ -1,7 +1,13 @@
 package at.sw2015.teamturingapp.uitest;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.util.Log;
+import android.widget.ImageView;
+
 import com.robotium.solo.Solo;
+
+import java.util.ArrayList;
+
 import at.sw2015.teamturingapp.MainActivity;
 import at.sw2015.teamturingapp.R;
 
@@ -40,8 +46,39 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
 
         boolean isVisible_half_r = mySolo.getCurrentActivity().getResources().getDrawable(R.mipmap.half_r).isVisible();
         assertTrue(isVisible_half_r);
+    }
+
+    public void testStartViews(){
+
+        ArrayList<ImageView> current_ImageView =  mySolo.getCurrentViews(ImageView.class);
+        Log.d("test", "OUUUT: " + current_ImageView.size());
+
+        if (current_ImageView.size() >= 11){
+
+            assertTrue(current_ImageView.contains(mySolo.getCurrentActivity().findViewById(R.id.border_l)));
+            assertTrue(current_ImageView.contains(mySolo.getCurrentActivity().findViewById(R.id.field1)));
+            assertTrue(current_ImageView.contains(mySolo.getCurrentActivity().findViewById(R.id.field2)));
+            assertTrue(current_ImageView.contains(mySolo.getCurrentActivity().findViewById(R.id.field3)));
+            assertTrue(current_ImageView.contains(mySolo.getCurrentActivity().findViewById(R.id.field4)));
+            assertTrue(current_ImageView.contains(mySolo.getCurrentActivity().findViewById(R.id.field5)));
+            assertTrue(current_ImageView.contains(mySolo.getCurrentActivity().findViewById(R.id.field6)));
+            assertTrue(current_ImageView.contains(mySolo.getCurrentActivity().findViewById(R.id.field7)));
+            assertTrue(current_ImageView.contains(mySolo.getCurrentActivity().findViewById(R.id.field8)));
+            assertTrue(current_ImageView.contains(mySolo.getCurrentActivity().findViewById(R.id.field9)));
+            assertTrue(current_ImageView.contains(mySolo.getCurrentActivity().findViewById(R.id.border_r)));
+
+        }
+        else
+        {
+            Log.d("test", "Test Failed");
+            assertTrue(false);
+        }
+
+
+
 
     }
+
 
 
 }
