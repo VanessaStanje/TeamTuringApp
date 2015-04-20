48c8ced9d3272a91488a158cf1f5324c6b03f467
VanessaStanje <vanessastanje@gmail.com>
2015-04-20 19:21:27 +0200

    [LG,VS] (test) Picture visible

--- app/src/androidTest/java/at/sw2015/teamturingapp/uitest/MainActivityTest.java
+++ app/src/androidTest/java/at/sw2015/teamturingapp/uitest/MainActivityTest.java
@@ -1,12 +1,10 @@
 package at.sw2015.teamturingapp.uitest;
 
 import android.test.ActivityInstrumentationTestCase2;
-
 import com.robotium.solo.Solo;
-
-import junit.framework.TestCase;
-
 import at.sw2015.teamturingapp.MainActivity;
+import at.sw2015.teamturingapp.R;
+
 
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
@@ -27,7 +25,23 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
     }
 
     public void testButtons(){
+        mySolo.clickOnButton("Step");
         mySolo.clickOnButton("Reset");
         mySolo.clickOnButton("Show");
     }
+
+    public void testStartViewsVisible(){
+
+        boolean isVisible_half_l = mySolo.getCurrentActivity().getResources().getDrawable(R.mipmap.half_l).isVisible();
+        assertTrue(isVisible_half_l);
+
+        boolean isVisible_base = mySolo.getCurrentActivity().getResources().getDrawable(R.mipmap.base).isVisible();
+        assertTrue(isVisible_base);
+
+        boolean isVisible_half_r = mySolo.getCurrentActivity().getResources().getDrawable(R.mipmap.half_r).isVisible();
+        assertTrue(isVisible_half_r);
+
+    }
+
+
 }
--- app/src/main/java/at/sw2015/teamturingapp/MainActivity.java
+++ app/src/main/java/at/sw2015/teamturingapp/MainActivity.java
@@ -6,12 +6,16 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
+
 public class MainActivity extends ActionBarActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
 
@@ -36,4 +40,5 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
