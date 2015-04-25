77cb7fb6536e1a183523706c4b83e56dfb05dc94
Lukas Gregori <lukas.gregori@student.tugraz.at>
2015-04-25 11:55:38 +0200

    [VS,LG] Implemented the readRawXMLInput Method within the parser

--- app/src/main/java/at/sw2015/teamturingapp/XMLParser.java
+++ app/src/main/java/at/sw2015/teamturingapp/XMLParser.java
@@ -2,7 +2,10 @@ package at.sw2015.teamturingapp;
 
 import java.io.IOException;
 import java.io.InputStream;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xmlpull.v1.XmlPullParserException;
 
@@ -14,6 +17,12 @@ public class XMLParser {
     public org.w3c.dom.Document readRawXMLInput(InputStream input_stream)
             throws XmlPullParserException, IOException,
             ParserConfigurationException, SAXException {
-        return null;
+        org.w3c.dom.Document document;
+        DocumentBuilderFactory db_factory = DocumentBuilderFactory
+                .newInstance();
+        DocumentBuilder db = db_factory.newDocumentBuilder();
+        InputSource input_source = new InputSource(input_stream);
+        document = db.parse(input_source);
+        return document;
     }
 }
