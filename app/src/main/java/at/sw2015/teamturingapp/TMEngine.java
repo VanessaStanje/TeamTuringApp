4c08d1b3b429c7dfc6db1acfa1212cef9def8026
Lukas Gregori <lukas.gregori@student.tugraz.at>
2015-05-03 14:55:46 +0200

    [VS,LG] Added the move head method of the tm engine

--- app/src/main/java/at/sw2015/teamturingapp/TMEngine.java
+++ app/src/main/java/at/sw2015/teamturingapp/TMEngine.java
@@ -1,7 +1,7 @@
 package at.sw2015.teamturingapp;
 
 /**
- * Created by grege on 03.05.15.
+ * Created by Lukas Gregori and Vanessa Stanje on 03.05.15.
  */
 public class TMEngine {
 
@@ -11,6 +11,11 @@ public class TMEngine {
 
     public int moveHead(int current_head_position, String direction)
     {
+        if (direction.equalsIgnoreCase("R"))
+            return current_head_position + 1;
+        else if (direction.equalsIgnoreCase("L"))
+            return current_head_position - 1;
+
         return current_head_position;
     }
 
