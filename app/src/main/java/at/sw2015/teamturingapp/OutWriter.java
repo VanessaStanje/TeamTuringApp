e2dbbd0e23e4f6b28c12d6a8847189c5f86392bf
Lukas Gregori <lukas.gregori@student.tugraz.at>
2015-05-23 10:15:40 +0200

    [LG,VS] Added the new OutWriter class (Output)

--- app/src/main/java/at/sw2015/teamturingapp/OutWriter.java
+++ app/src/main/java/at/sw2015/teamturingapp/OutWriter.java
@@ -15,9 +15,41 @@ public class OutWriter {
 
     public  OutWriter(String directory)
     {
+        this.directory = directory;
     }
 
-    public boolean writeXMLToFile(org.w3c.dom.Document content,String file_name) {
+    public boolean writeXMLToFile(org.w3c.dom.Document content,String file_name){
+        try {
+
+            if(directory.length() == 0) {
+                System.err.println("ERROR; NO DIRECTORY SELECTED!");
+                return false;
+            }
+
+            File newFolder = new File(Environment.
+                    getExternalStorageDirectory(), directory);
+            if (!newFolder.exists() && !newFolder.mkdir()) {
+                System.err.println("ERROR; COULD NOT CREATE FOLDER!");
+                return false;
+            }
+
+            File file = new File(newFolder, file_name + ".xml");
+            if(!file.exists() && !file.createNewFile())
+            {
+                System.err.println("ERROR; COULD NOT CREATE FILE!");
+                return false;
+            }
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource domSource = new DOMSource(content);
+            StreamResult streamResult = new StreamResult(file);
+            transformer.transform(domSource, streamResult);
+
+        } catch (Exception e) {
+            System.out.println("ERROR; " + e);
+            return false;
+        }
         return true;
     }
 }
